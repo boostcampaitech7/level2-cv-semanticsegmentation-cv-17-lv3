b985c82e18965a4b3449f17cfe2f5b300c48bdb6
yookyung <susan6439@naver.com>
2024-11-16 01:26:11 +0000

    [fix] #14 - state_dict() 부분 수정

--- code/train.py
+++ code/train.py
@@ -56,7 +56,7 @@ class ModelCheckpoint:
             os.makedirs(SAVED_DIR)
 
     def save_ckpt(self, model, path):
-        torch.save(model.state_dict(), path)
+        torch.save(model, path)
 
     def delete_ckpt(self):
         dice_remove, epoch_remove, path_remove = self.best_models.pop(-1)
@@ -91,7 +91,7 @@ class ModelCheckpoint:
         # 가장 높은 dice 모델 저장
         best_model_path = os.path.join(SAVED_DIR, 'best_model.pt')
         best_dice, best_epoch, _ = self.best_models[0]
-        torch.save(model.state_dict(), best_model_path)
+        torch.save(model, best_model_path)
         print(f"Best model saved for epoch {best_epoch+1} with highest dice = {best_dice:.4f}")
 
 def set_seed():
